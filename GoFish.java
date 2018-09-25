1c59a0b0f2830c8e182bf155fc5132d992ecd025
William Slocum <wslocum@uvm.edu>
2018-09-24 23:46:42 -0400

    Some Preliminary Sudo Code for Game Logic

--- GoFish.java
+++ GoFish.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class GoFish
 {
   //FIELDS
-  public static Player[] Players;
-  public static Collection Deck;
+  public static Player[] players;
+  public static Collection deck;
+
+  public static void switchTurn()
+  {
+    //switch turns somehow
+  }
+
+  public static Boolean isGameOver()
+  {
+    //game is over when all cards are in completed sets of 4
+    //thus, game is over when there are 52/4=13 sets
+    if(//humanPlayer.sets + computerPlayer.sets == 13)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+
+  }
 
   public static void main(String[] args)
   {
@@ -23,8 +45,45 @@ public class GoFish
      *        - otherwsise, current player's turn ends
      */
 
+     //initialize scanner object
+     Scanner Reader = new Scanner(System.in);
+
+     //initalize boolean to determine if current player get an additional turns
+     Boolean playAgain;
+
+
+     while(!isGameOver())
+     {
+       playAgain = true;
+
+       while(playAgain)
+       {
+          //if current player is human, call Player.showHand()
+          //if current player is human, ask for a card, accept input
+          //run checkHand() on non-current player
+          if(//checkHand==true)
+          {
+            //switch card from hands
+            playAgain = true;
+          }
+          else
+          {
+            //current player draws from deck
+            if(//drawn card matches card asked for)
+            {
+              //check for sets of 4
+              playAgain = true;
+            }
+            else
+            {
+              //check for sets of 4
+              playAgain = false;
+            }
+          }
+      }
+
+      switchTurns();
+    }
+
+    //Display some output at the end of the game
   }
-
-
-
-}
